56b02fa2ef1df5b5b80e8f43f98419fea2c86cb8
LeviG99 <levi.gomes@ccc.ufcg.edu.br>
2019-06-12 20:26:37 -0300

    Usuario, agora com os atributos, gets e sets.

--- src/main/java/com/backend/model/Usuario.java
+++ src/main/java/com/backend/model/Usuario.java
@@ -1,5 +1,38 @@
 package com.backend.model;
 
 public class Usuario {
-
+	private String primeiroNome;
+	private String ultimoNome;
+	private String email;
+	private String password;
+	public Usuario (String primeiroNome,String ultimoNome,String email,String password) {
+		this.primeiroNome = primeiroNome;
+		this.ultimoNome = ultimoNome;
+		this.email = email;
+		this.password = password;
+	}
+	public String getPrimeiroNome() {
+		return primeiroNome;
+	}
+	public void setPrimeiroNome(String primeiroNome) {
+		this.primeiroNome = primeiroNome;
+	}
+	public String getUltimoNome() {
+		return ultimoNome;
+	}
+	public void setUltimoNome(String ultimoNome) {
+		this.ultimoNome = ultimoNome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
 }
