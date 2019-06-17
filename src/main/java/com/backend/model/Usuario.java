74757b6db31f7aaf80e93f2e2e1cf2ebc9568406
LeviG99 <levi.gomes@ccc.ufcg.edu.br>
2019-06-17 14:40:15 -0300

    postman funcionando

--- src/main/java/com/backend/model/Usuario.java
+++ src/main/java/com/backend/model/Usuario.java
@@ -5,40 +5,50 @@ import javax.persistence.Id;
 
 @Entity
 public class Usuario {
-	private String primeiroNome;
-	private String ultimoNome;
-	
 	@Id
 	private String email;
-	
 	private String password;
-	public Usuario (String primeiroNome,String ultimoNome,String email,String password) {
+	private String primeiroNome;
+	private String ultimoNome;
+
+	public Usuario() {
+	}
+
+	public Usuario(String primeiroNome, String ultimoNome, String email, String password) {
 		this.primeiroNome = primeiroNome;
 		this.ultimoNome = ultimoNome;
 		this.email = email;
 		this.password = password;
 	}
+
 	public String getPrimeiroNome() {
 		return primeiroNome;
 	}
+
 	public void setPrimeiroNome(String primeiroNome) {
 		this.primeiroNome = primeiroNome;
 	}
+
 	public String getUltimoNome() {
 		return ultimoNome;
 	}
+
 	public void setUltimoNome(String ultimoNome) {
 		this.ultimoNome = ultimoNome;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
